b72b5717d29026a1a17859a61ed492a562567509
santhosh-13022 <santhosh.110999@gmail.com>
2022-03-09 11:46:25 +0530

    Added More Methods

--- src/service/CarParkingFunctionalities.java
+++ src/service/CarParkingFunctionalities.java
@@ -1,15 +1,31 @@
 package service;
 
+import model.Car;
+import model.ParkingLot;
+
 public interface CarParkingFunctionalities {
 
-    void parkACar();
+    Car acceptCarDetailsToPark();
 
-    void exitACar();
+    boolean confirmCarDetails(Car car);
+
+    boolean checkAndSuggestCarParkingFloor(Car car);
+
+    ParkingLot suggestAndGetCarParkingLot();
+
+    int[] suggestAndGetParkingPlace(ParkingLot parkingLot);
+
+    void generateReceipt(ParkingLot parkingLot, int[] pos, Car car);
+
+    Car acceptCarDetailsToExit();
+
+    boolean confirmCarDetailsForExit(Car car);
+
+    void generateBill(ParkingLot parkingLot, int[] pos, Car car);
 
     void showAllParkingSlots();
 
     void showAllDetailedParkingSlots();
 
     void getCarHistory();
-
 }
