4d127e996c4a3b8dae7fa168b2abda555efd7dd1
santhosh-13022 <santhosh.110999@gmail.com>
2022-03-09 11:56:13 +0530

    Calling the methods and manipulating the result here

--- src/service/CarParkingMenu.java
+++ src/service/CarParkingMenu.java
@@ -1,23 +1,52 @@
 package service;
 
-import util.Validation;
+import model.Car;
+import model.MultiFloorCarParking;
+import model.ParkingLot;
+import model.PropertiesClass;
+import util.CarValidator;
+import util.Validator;
 
 import java.util.Scanner;
 
 public class CarParkingMenu {
 
-    static Scanner in = new Scanner(System.in);
-    static CarParkingMessage app = new CarParkingMessage();
-    public static void showMenu(CarParkingFunctionalities appFunctionalities) {
+    private final Scanner in;
+    private final CarParkingMessage msg;
+
+    private final MultiFloorCarParking obj;
+    private final CarParkingFunctionalities appFunctionalities;
+
+    public CarParkingMenu(PropertiesClass prop) {
+        in = new Scanner(System.in);
+        msg = new CarParkingMessage();
+        obj = new MultiFloorCarParking(prop);
+        appFunctionalities = new CarParkingFunctionalitiesImpl(obj);
+    }
+
+    public void showMenu() {
         while (true) {
-            app.showMenu();
+            msg.showMenu();
             String ch = in.nextLine().trim();
-            int choice = Validation.validateInteger(ch,1,6);
+            int choice = Validator.validateInteger(ch,1,6);
             if(choice == 1) {
-                appFunctionalities.parkACar();
+                Car car = appFunctionalities.acceptCarDetailsToPark();
+                if(car == null) continue;
+                if(CarValidator.checkDuplicateCarNoInParking(car.getCarNumber())) continue;
+                if(!appFunctionalities.confirmCarDetails(car)) continue;
+                if(appFunctionalities.checkAndSuggestCarParkingFloor(car)) continue;
+                ParkingLot parkingLot = appFunctionalities.suggestAndGetCarParkingLot();
+                int[] pos = appFunctionalities.suggestAndGetParkingPlace(parkingLot);
+                appFunctionalities.generateReceipt(parkingLot,pos,car);
             }
             else if(choice == 2) {
-                appFunctionalities.exitACar();
+                Car car = appFunctionalities.acceptCarDetailsToExit();
+                if(car == null) continue;
+                String carNo = car.getCarNumber();
+                if(!appFunctionalities.confirmCarDetailsForExit(car)) continue;
+                ParkingLot parkingLot = obj.getParkingLotWithCarNumber(carNo);
+                int[] pos = parkingLot.getCarNumberPosition(carNo);
+                appFunctionalities.generateBill(parkingLot,pos,car);
             }
             else if(choice == 3) {
                 appFunctionalities.showAllParkingSlots();
@@ -32,6 +61,11 @@ public class CarParkingMenu {
                 break;
             }
         }
+    }
+
+    private void hashLine() {
+        System.out.println();
+        System.out.println("#".repeat(170));
     }
 
 }
