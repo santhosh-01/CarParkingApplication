d0cd8e03f470d53f1b4b9f5da39e9a287571b6a9
santhosh-13022 <santhosh.110999@gmail.com>
2022-03-15 11:10:03 +0530

    Removed unused methods and imports

--- src/service/CarParkingMessage.java
+++ src/service/CarParkingMessage.java
@@ -1,9 +1,5 @@
 package service;
 
-import model.Car;
-import model.ParkingLot;
-import util.OrdinalNumber;
-
 public class CarParkingMessage {
 
     public CarParkingMessage() {
@@ -25,28 +21,8 @@ public class CarParkingMessage {
         System.out.print("Please Choose any of the above option: ");
     }
 
-    public void showParkingPlace(ParkingLot parkingLot, int[] pos) {
-        System.out.println("\nCar Parking Place : " + (pos[0] + 1) + "/" + (pos[1] + 1) + " at " +
-                getOrdinalNumber(parkingLot.getFloorNo()) + " floor");
-    }
-
-    public void showParkingSuccess(ParkingLot parkingLot, Car car) {
-        System.out.println("\nCar Number " + car.getCarNumber() + " parked successfully in " +
-                getOrdinalNumber(parkingLot.getFloorNo()) + " floor ");
-    }
-
-    public void showCarExitSuccess(ParkingLot parkingLot, Car car) {
-        System.out.println("\nCar Number " + car.getCarNumber() + " removed successfully from " +
-                getOrdinalNumber(parkingLot.getFloorNo()) + " floor ");
-    }
-
     public void quitMessage() {
         System.out.println("\n########## Thank you for using the Application ##########");
-    }
-
-    private String getOrdinalNumber(int floorNo) {
-        if(floorNo == 0) return "Ground";
-        else return OrdinalNumber.getOrdinalNo(floorNo);
     }
 
 }
